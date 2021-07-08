1e22aa95373667f4079f5fe7d82cc46086a8b101
Carlo von Reyher <c.v.reyher@gmail.com>
2021-07-08 14:18:27 +0200

    Data fetching done

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+// Map Initialisation
+
 var map = L.map('map').setView([40, -15], 3);
 
 const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
@@ -10,3 +12,26 @@ L.tileLayer(tileURL, {
 
 // Set the view point to auto
 map.locate({setView: true, maxZoom: 9});
+
+// Fetching data from API
+
+const apiKEY = "0sJDTpkjL9xgHnZZTT9vajCQfnPnlvTIx"
+
+fetchdata();
+
+async function fetchdata (){
+    const apiURL = 'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/' + apiKEY + '/message.json';
+    const response = await fetch(apiURL)
+    const data = await response.json().then(function(data){
+        console.log(data)
+    });
+}
+
+
+    // for ( item of data.response.messages.message) {
+    
+        
+    //     const marker = L.marker([item.latitude, item.longitude]).addTo(map);
+    
+    //     // marker.bindPopup('<h1 class="title">'+ item.name + '</h1><p class="text">' + item.description +'</p><br><img width="100%" class="image" src="' + item.imageurl + '" /><br><a class="btn" href="' + item.link + '">Website</a><br />');
+    // }
