053e82d0beb24e19d7d74e1c3f6180cbb0f02907
Carlo von Reyher <c.v.reyher@gmail.com>
2021-07-12 21:49:03 +0200

    Working Navigation

--- Website/public/mobilenavigation.js
+++ Website/public/mobilenavigation.js
@@ -4,6 +4,7 @@ var closebuttonnavigationMobile = document.getElementById('closeButton')
 // Adding the Animation Timeline from GSAP
 var animationOpenMenuMobile = gsap.timeline()
 .to("#overlaywrapper", {duration: .5, x: 0, opacity: 1})
+.to("#closeButton", {duration: .5, y: 0, opacity: 1})
 .to("#overlaywrapper div ul li", {duration: .3, stagger:0.1, y: 0, ease:"back", opacity: 1});
 
 // Stopping the Timeline from starting automaticaly
