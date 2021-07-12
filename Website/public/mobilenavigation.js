13a1a29945e658c3a22ff412f3201e1721362615
Carlo von Reyher <c.v.reyher@gmail.com>
2021-07-12 21:30:09 +0200

    Mobile Nav done

---
+++ Website/public/mobilenavigation.js
@@ -0,0 +1,21 @@
+var buttonopennavigationmobile = document.getElementById("openbutton");
+var closebuttonnavigationMobile = document.getElementById('closeButton')
+
+// Adding the Animation Timeline from GSAP
+var animationOpenMenuMobile = gsap.timeline()
+.to("#overlaywrapper", {duration: .5, x: 0, opacity: 1})
+.to("#overlaywrapper div ul li", {duration: .3, stagger:0.1, y: 0, ease:"back", opacity: 1});
+
+// Stopping the Timeline from starting automaticaly
+animationOpenMenuMobile.pause();
+
+// Adding Event Listener for Case of Click
+buttonopennavigationmobile.addEventListener("click", ()=> {
+    animationOpenMenuMobile.play();
+})
+
+closebuttonnavigationMobile.addEventListener('click', () => {
+    animationOpenMenuMobile.reverse();
+})
+
+
