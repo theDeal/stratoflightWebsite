86bb9c2df60fc02bee5a458d74a0bb3531b3022d
Carlo von Reyher <c.v.reyher@gmail.com>
2021-07-12 17:14:54 +0200

    Added Footer to the Website

--- Website/public/homepage.js
+++ Website/public/homepage.js
@@ -1,6 +1,6 @@
 // Countdown
 // Set the date we're counting down to
-var countDownDate = new Date("Tue, 13 Jul 2021 11:39:57 +0000").getTime();
+var countDownDate = new Date("Tue, 20 Jul 2021 11:39:57 +0000").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
